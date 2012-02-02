9eb2c00aa139b4f01caade163af3ec6b37b03457
Scott Smith <scott@disqus.com>
2012-02-01 20:54:37 -0800

    Move caching into bokbok.lib.metrics module.

--- bokbok/lib/metrics.py
+++ bokbok/lib/metrics.py
@@ -1,4 +1,21 @@
+from werkzeug.contrib.cache import SimpleCache
+
+from bokbok import graphite_host
+
+cache = SimpleCache()
+CACHE_TIMEOUT = 86400
+
 class Metrics(object):
+
+    def cache(self):
+        import json
+        import requests
+        from redis import Redis
+
+        redis = Redis()
+        r = requests.get('http://%s/metrics/index.json' % graphite_host)
+        for line in json.loads(r.text):
+            redis.sadd('bokbok:metrics_cache', line.lstrip('.'))
 
     def index(self, data):
         index = dict()
@@ -7,4 +24,15 @@ class Metrics(object):
         return index
 
     def find(self, query, index):
+        from flask import g
+
+        index = cache.get('bokbok:metrics_index')
+
+        if not cache.get('bokbok:metrics_list'):
+            metrics_list = list(g.redis.smembers('bokbok:metrics_cache'))
+            cache.set('bokbok:metrics_list', metrics_list, CACHE_TIMEOUT)
+        if not index:
+            index = self.index(metrics_list)
+            cache.set('bokbok:metrics_index', index, CACHE_TIMEOUT)
+
         return [metric for metric in sorted(index) if query in metric]
--- bokbok/views/frontend.py
+++ bokbok/views/frontend.py
@@ -1,13 +1,11 @@
 import json
 from flask import Module, g, jsonify, render_template, request
-from werkzeug.contrib.cache import SimpleCache
+
 
 from bokbok import graphite_host, graphite_options
 from bokbok.lib.metrics import Metrics
 
 frontend = Module(__name__)
-cache = SimpleCache()
-CACHE_TIMEOUT = 86400
 
 @frontend.route('/')
 def index():
@@ -19,13 +17,5 @@ def index():
 def metrics():
     query = request.args.get('query', '')
     metrics = Metrics()
-    index = cache.get('bokbok:metrics_index')
-
-    if not cache.get('bokbok:metrics_list'):
-        metrics_list = list(g.redis.smembers('metrics'))
-        cache.set('bokbok:metrics_list', metrics_list, CACHE_TIMEOUT)
-    if not index:
-        index = metrics.index(metrics_list)
-        cache.set('bokbok:metrics_index', index, CACHE_TIMEOUT)
 
     return jsonify(message=metrics.find(query, index)[:15])
